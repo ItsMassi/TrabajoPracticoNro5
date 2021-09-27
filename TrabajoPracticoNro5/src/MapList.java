9d28045c1c7360d021590692ee5191f2b00889ea
ItsMassi <danielmassiris10@gmail.com>
2021-09-27 08:44:34 -0300

    Add: MapList Methods, must implement, positionList, Position and exceptions

--- TrabajoPracticoNro5/src/MapList.java
+++ TrabajoPracticoNro5/src/MapList.java
@@ -1,7 +1,59 @@
 
 
-public class MapList {
-    public MapList(){
-           System.out.println();  
-    }
+public class MapList<K,V> implements Map<K,V> {
+	
+	protected PositionList<Entrada<K,V>> m;
+	
+	public MapList() {
+		
+	}
+	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<K> keys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<V> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Entry<K, V>> entries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
 }
