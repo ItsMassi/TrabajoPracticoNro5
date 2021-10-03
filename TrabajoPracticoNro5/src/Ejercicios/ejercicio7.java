7119db3beecf7dab51288eeffcf3920b1bbcd624
ItsMassi <danielmassiris10@gmail.com>
2021-10-03 16:06:02 -0300

    Add: tiempo de ejecucion del ejercicio 7

--- TrabajoPracticoNro5/src/Ejercicios/ejercicio7.java
+++ TrabajoPracticoNro5/src/Ejercicios/ejercicio7.java
@@ -8,24 +8,27 @@ public class ejercicio7<K,V> {
 	public ejercicio7() {}
 	public boolean incisoA(Map<K,V> m1, Map<K,V> m2) {
 		System.out.println(m1.size()<=m2.size());
-		boolean retorno = m1.size()<=m2.size();
-		V valor = null;
-		Iterable<K> itera = m1.keys();
+		boolean retorno = m1.size()<=m2.size(); //size()-> O(1) por tanto esto es c1
+		V valor = null; //c2
+		Iterable<K> itera = m1.keys();//keys tiene O(n) porque require recorrer todo el mapeo para hacer el iterable
 				
-		Iterator<K> it = itera.iterator();
-		while(it.hasNext() && retorno) {
+		Iterator<K> it = itera.iterator(); //O(n) por la misma razon que keys
+		while(it.hasNext() && retorno) {//en el peor caso hace n iteraciones 
 			try {
-				valor = m2.get(it.next());
+				valor = m2.get(it.next());//en el peor caso hace m iteraciones
 			} catch (InvalidKeyException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			retorno = valor != null;
+			retorno = valor != null ;//c3
 		}
 		
-		return retorno;
+		return retorno;//c4
 	}
+	/*entonces T(n)= c1 + o(n) o(n) + (n*(o(1)+O(m))+O(1))
+	 *         T(n)= (n*o(1)+n*O(m)+O(1))
+	 *         T(n)= n*O(m)*/
 	
 	public static void main(String[] args) {
 		ejercicio7<String,Integer> ej =  new ejercicio7<String,Integer>();
